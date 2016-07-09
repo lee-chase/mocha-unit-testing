b4a64b84fcdfab28aead3c6d2f49719be10e5961
lee-chase <lee.chase@uk.ibm.com>
2016-07-09 15:07:23 +0100

    some of my own tests

--- js/className.js
+++ js/className.js
@@ -1,16 +1,29 @@
 // Browser version
-function addClass(el, newClass) {
-  if(el.className.indexOf(newClass) !== -1) {
-    return;
-  }
+function addClass(el, classname) {
+  el.classList.add(classname);
+};
 
-  if(el.className !== '') {
-    //ensure class names are separated by a space
-    newClass = ' ' + newClass;
-  }
+function removeClass(el, classname) {
+  el.classList.remove(classname);
+};
 
-  el.className += newClass;
-}
+function toggleClass(el, classname, toState) {
+  el.classList.toggle(classname, toState);
+};
+
+// Original browser
+// function addClass(el, newClass) {
+//   if(el.className.indexOf(newClass) !== -1) {
+//     return;
+//   }
+//
+//   if(el.className !== '') {
+//     //ensure class names are separated by a space
+//     newClass = ' ' + newClass;
+//   }
+//
+//   el.className += newClass;
+// }
 
 // Node Version
 // module.exports = {
--- test/classNameTest.js
+++ test/classNameTest.js
@@ -5,30 +5,92 @@ var assert = chai.assert;
 // var className = require('../js/className.js');
 // var addClass = className.addClass;
 
+var TEST_CLASS = 'test-class';
+var NEW_CLASS = 'new-class'
+
 describe('addClass', function() {
+  var element;
+
+  beforeEach(function(done) {
+    element = document.createElement('div');
+    done();
+  });
+
   it('should add class into element', function() {
-    var element = { className: '' };
+    addClass(element, TEST_CLASS);
 
-    addClass(element, 'test-class');
-
-    assert.equal(element.className, 'test-class');
+    assert.equal(element.classList.contains(TEST_CLASS), true);
   });
 
   it('should not add a class which already exists in element', function() {
-    var element = { className: 'exists' };
+    addClass(element, TEST_CLASS);
+    addClass(element, TEST_CLASS);
 
-    addClass(element, 'exists');
-
-    var numClasses = element.className.split(' ').length;
+    var numClasses = element.classList.length;
     assert.equal(numClasses, 1);
   });
 
   it('should append new class after existing one', function() {
-    var element = { className: 'exists' };
+    addClass(element, TEST_CLASS);
+    addClass(element, NEW_CLASS);
 
-    addClass(element, 'new-class');
+    assert.equal(element.classList.contains(TEST_CLASS), true);
+    assert.equal(element.classList.contains(NEW_CLASS), true);
+  });
+});
 
-    var classes = element.className.split(' ');
-    assert.equal(classes[1], 'new-class');
+describe('removeClass', function() {
+  var element;
+
+  beforeEach(function(done) {
+    element = document.createElement('div');
+    element.classList.add(TEST_CLASS);
+    done();
+  });
+
+  it('should remove a class which already exists', function() {
+    removeClass(element, TEST_CLASS);
+
+    assert.equal(element.classList.contains(TEST_CLASS), false);
+  });
+
+  it('should not affect other classes', function() {
+    removeClass(element, NEW_CLASS);
+
+    assert.equal(element.classList.contains(TEST_CLASS), true);
+  });
+});
+
+describe('toggleClass', function() {
+  var element;
+
+  beforeEach(function(done) {
+    element = document.createElement('div');
+    element.classList.add(TEST_CLASS);
+    done();
+  });
+
+  it('should remove a class that exists when no state is specified', function() {
+    toggleClass(element, TEST_CLASS);
+
+    assert.equal(element.classList.contains(TEST_CLASS), false);
+  });
+
+  it('should add a class that exists when no state is specified', function() {
+    toggleClass(element, NEW_CLASS);
+
+    assert.equal(element.classList.contains(NEW_CLASS), true);
+  });
+
+  it('should add / keep class if toState is true', function() {
+    toggleClass(element, TEST_CLASS, true);
+
+    assert.equal(element.classList.contains(TEST_CLASS), true);
+  });
+
+  it('should remove / not add a class if toState is false', function() {
+    toggleClass(element, NEW_CLASS, false);
+
+    assert.equal(element.classList.contains(NEW_CLASS), false);
   });
 });
